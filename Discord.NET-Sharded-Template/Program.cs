15a9f0f221ac6ebeddc0b0c53e042cf7dd9be46c
Directoire <hendrikdemir@protonmail.com>
2021-06-14 14:51:24 +0200

    Add project files.

---
+++ Discord.NET-Sharded-Template/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Discord.NET_Sharded_Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
